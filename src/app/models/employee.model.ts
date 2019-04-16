35d77b8b0ab9faf2e7fbb664ed04a12488228198
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-04-16 16:40:24 +0100

    Added two way databinding to check box - boolean

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -1,6 +1,7 @@
 export class Employee {
   constructor(public firstName: string,
-              public lastName: string){
+              public lastName: string,
+              public isFullTime: boolean){
 
   }
 }

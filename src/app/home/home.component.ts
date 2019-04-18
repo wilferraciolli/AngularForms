0e238fa6b0b4d7a03096fa6f0a65ead99ed20380
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-04-18 10:30:45 +0100

    Added ability to validate a select component using the comoponent logic used the blur event to validate an inout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,28 @@ import {Employee} from "../models/employee.model";
 export class HomeComponent {
 
   languages = ['English', 'Greek', 'Other'];
-  model = new Employee('Wiliam', 'Ferraciolli', true, 'w2', 'English')
+  model = new Employee('', '', true, '', 'default')
+  hasPrimaryLanguageError = false;
 
-  firstNameToUpperCase(value: string){
-    if (value.length > 0){
+  /**
+   * Function to check whether a value other than the default was chosen.
+   * This is to validate the select model
+   * @param event
+   */
+  validatePrimaryLanguage(event) {
+    console.log('lang ' + this.model.primaryLanguage);
+    if (this.model.primaryLanguage == 'default') {
+      this.hasPrimaryLanguageError = true;
+    } else {
+      this.hasPrimaryLanguageError = false;
+    }
+  }
+
+  firstNameToUpperCase(value: string) {
+    if (value.length > 0) {
       //make the first letter capital
       this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
-    }else{
+    } else {
       this.model.firstName = value;
     }
 
